efb343cf2c9da749f34bf5f3346952d71a982447
barlima <bartekperucki@gmail.com>
2018-11-23 00:13:10 +0100

    Refactor TitleBracketsValidator

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,67 +1,56 @@
 class TitleBracketsValidator < ActiveModel::Validator
 
+  BRECKETS_PAIRS = [
+    ['(', ')'],
+    ['[', ']'],
+    ['{', '}']
+  ]
+
   def validate(record)
-    unless record.title.count('(') == record.title.count(')')
-      record.errors[:title] << 'has invalid number of parantheses'
-    end
-
-    unless record.title.count('{') == record.title.count('}')
-      record.errors[:title] << 'has invalid number of braces'
-    end
-
-    unless record.title.count('[') == record.title.count(']')
-      record.errors[:title] << 'has invalid number of square brackets'
-    end
-
-    unless check_order(record.title, '(', ')')
-      record.errors[:title] << 'has invalid parantheses order'
-    end
-
-    unless check_order(record.title, '{', '}')
-      record.errors[:title] << 'has invalid braces order'
-    end
-
-    unless check_order(record.title, '[', ']')
-      record.errors[:title] << 'has wrong square brackets order'
-    end
-
-    unless check_empty(record.title)
-      record.errors[:title] << 'contains empty brackets'
-    end
+    check_number(record)
+    check_order(record)
+    check_empty(record)
   end
 
-  def check_order(title, opening, closing)
-    sum = 0
-    title.split('').each do |c|
-      if c == opening
-        sum += 1
-      elsif c == closing
-        sum -= 1
+  private
+
+  def check_number(record)
+    BRECKETS_PAIRS.each do |opening, closing|
+      unless record.title.count(opening) == record.title.count(closing)
+        record.errors[:title] << "has invalid number of #{opening}#{closing}"
       end
-      return false if sum < 0
     end
-    true
   end
 
-  def check_empty(title)
+  def check_order(record)
+    BRECKETS_PAIRS.each do |opening, closing|
+      sum = 0
+      record.title.split('').each do |c|
+        if c == opening
+          sum += 1
+        elsif c == closing
+          sum -= 1
+        end
+        record.errors[:title] << "has invalid #{opening}#{closing}" if sum < 0
+      end
+    end
+  end
+
+  def check_empty(record)
     content = ""
     opened = false
-    [
-      ['(', ')'], 
-      ['[', ']'], 
-      ['{', '}']
-    ].each do |opening, closing|
-      title.split('').each do |c|
+
+    BRECKETS_PAIRS.each do |opening, closing|
+      record.title.split('').each do |c|
         if c == opening
           opened = true
         elsif c == closing
-          return false if content.blank? && opened
+          record.errors[:title] << 'contains empty brackets' if content.blank? && opened
           opened = false
         elsif opened
           content << c unless ( %w( ( ) [ ] { } ).include?(c) )
         end
       end
     end
-    true
   end
 end
